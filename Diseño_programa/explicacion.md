6e5c2d4b4b8cac2bf2fc6380c62e4ed84a3b362b
Ubuntu <msola@oop-msola.lwkvwpaqdp4epgkwmx1p4ovsxa.phxx.internal.cloudapp.net>
2022-09-13 14:31:18 +0000

    aaa profe porfavor acepteme

---
+++ Diseño_programa/explicacion.md
@@ -0,0 +1,12 @@
+## Explicacion programa
+
+    Lo que hara este programa es generarar una contraseña teniendo en cuenta la cantidad de numeros
+    la cantidad de minusculas y mayusculas, de acuerdo a esto se esepcificara la longitud de la contraseña 
+    que tendra introducido dentro de esto lo dicho anteriormente
+
+### explicacion uml
+    cree dos clases, la clase contraseña y la clase 
+    generador de contraseña, dentro de la clase contraseña estan los atirbutos  numeros,longitud,minusculas y mayusculas y tendra el metodo escoger donde el usuario seleccionara lo dicho en explicacion del programa, 
+    luego cree otra clase hija que hereda de contraseña
+    con la que cuenta con el atributo contraseña y el metodo
+    generador que lo que hara es generar la contraseña requerida.
--- Diseño_programa/generador.py
+++ Diseño_programa/generador.py
@@ -1,24 +1,31 @@
 import random
 import string
+from VALID import ver
 
-class generadorContraseña:
-    def __init__(self, longitud : int, contraseña : str, minusculas: str, mayusculas : str) :
-        self.contraseña = " "
+class Contraseña:
+    def __init__(self, longitud : int,numeros :int, minusculas: str, mayusculas : str) :
         self.longitud = 100
         self.minusculas = " "
         self.mayusculas = " "
+        self.numeros = 0
        
+    def escoger(self): 
+        self.longitud = ver (input("Introduzca la longitud de la contraseña: "))
+        self.minusculas =  ver (input("Introduzca el numero minimo de minusculas que quiere en la contraseña: "))
+        self.mayusculas =  ver (input("Introduzca el numero minimo de mayusculas que quiere en la contraseña: "))
+        self.numeros = ver (input("Introduzca la cantidad de numeros que quiere en la contraseña"))
+        caracteres = string.ascii_letters + string.digits
 
-    def escogerLongitud(self):
-        self.longitud = int(input("Introduzca la longitud de la contraseña: "))
-        
+class generarcontraseña(Contraseña):
+        def __init__(self, longitud: int, numeros: int, contraseña: str, minusculas: str, mayusculas: str, contraseña : str):
+            super().__init__(longitud, numeros, contraseña, minusculas, mayusculas)
+            self.contraseña = ""
 
-    def generarcontraseña(self):
-        contraseña = " "
-        ##declaracion de la contraseña
+        def generador(self):
+            while True:
+                contraseña = (" ").join(random.choice(caracteres) for i in range(longitud))
+                for num in range(lMaxima):
+                password = password + chr(int(random.randint(32, 126))) 
 
-        for num in range(lMaxima):
-          password = password + chr(int(random.randint(32, 126))) 
-
-        print('Clave Generada: ', contraseña)
+            print('Clave Generada: ', contraseña)
 
