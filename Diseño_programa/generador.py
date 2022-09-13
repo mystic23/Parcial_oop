9eaafecdd26438ce002fb67ede95ff67a856a95a
María Isabel Solá Valle <msola@uninorte.edu.co>
2022-09-13 10:08:21 -0500

    profe ya estaba pero no tenia internet y no se queria subir

--- Diseño_programa/generador.py
+++ Diseño_programa/generador.py
@@ -1,36 +1,38 @@
+
 import random
 import string
 from VALID import ver
-import subprocess
+
 
 class Contraseña:
-    def __init__(self, longitud : int,numeros :int, minusculas: str, mayusculas : str) :
-        self.longitud = 100
-        self.minusculas = " "
-        self.mayusculas = " "
-        self.numeros = 0
+    def __init__(self, longitud : int,numeros :int, minusculas: int, mayusculas : int) :
+        self.longitud = longitud
+        self.minusculas =  minusculas
+        self.mayusculas =  mayusculas
+        self.numeros = numeros
        
-    def escoger(self): 
-        self.longitud = ver (input("Introduzca la longitud de la contraseña: "))
-        self.minusculas =  ver (input("Introduzca el numero minimo de minusculas que quiere en la contraseña: "))
-        self.mayusculas =  ver (input("Introduzca el numero minimo de mayusculas que quiere en la contraseña: "))
-        self.numeros = ver (input("Introduzca la cantidad de numeros que quiere en la contraseña"))
-        caracteres = string.ascii_letters + string.digits
+    # def escoger(self): 
+    #     self.longitud = ver (input("Introduzca la longitud de la contraseña: "))
+    #     self.minusculas =  ver (input("Introduzca el numero minimo de minusculas que quiere en la contraseña: "))
+    #     self.mayusculas =  ver (input("Introduzca el numero minimo de mayusculas que quiere en la contraseña: "))
+    #     self.numeros = ver (input("Introduzca la cantidad de numeros que quiere en la contraseña"))
+        
 
 class generarcontraseña(Contraseña):
-        def __init__(self, longitud: int, numeros: int, contraseña: str, minusculas: str, mayusculas: str, contraseña : str):
-            super().__init__(longitud, numeros, contraseña, minusculas, mayusculas)
-            self.contraseña = ""
+        def __init__(self, longitud: int, numeros: int,  minusculas: int, mayusculas: int):
+            super().__init__(longitud, numeros, minusculas, mayusculas)
+            
 
-        def generador(self, esocger):
+        def generador(self):
             longitud = self.longitud
             caracteres = string.ascii_letters + string.digits
             while True:
                 contraseña = (" ").join(random.choice(caracteres) for i in range(longitud))
-                if(sum(c.islower()for c in contraseña)) >= minusculas
-                    and sum (c.isupper() for c in contraseña) >= mayusculas
-                    and sum(c.isdigit() for c in contraseña) >= numeros:
+                if(sum(c.islower() for c in contraseña)) >= self.minusculas and sum (c.isupper() for c in contraseña) >= self.mayusculas and sum(c.isdigit() for c in contraseña) >= self.numeros:
                     break
 
             print('La clave generada de acuerdo a sus indicaciones es ', contraseña)
+
+contraseña = generarcontraseña(8,5,1,2)
+contraseña.generador()
 
